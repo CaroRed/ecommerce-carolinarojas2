4f6557c86f4a38c0f642a1d1030883fe4e081218
Carolina Rojas <carorojas27@gmail.com>
2022-03-13 12:23:02 -0300

    Desafío: Menú e-commerce

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,43 @@
+import {Container , Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
+
+import './NavBar.css'
+
+function NavBar() {
+    return (
+    <Navbar bg="dark" expand="lg" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="#">React Ecommerce</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Inicio</Nav.Link>
+            <Nav.Link href="#action2">Tienda</Nav.Link>
+            <NavDropdown title="Categorías" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Figuras</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Mochilas</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Otros
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Buscar..."
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Buscar</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    )
+}
+
+export default NavBar
